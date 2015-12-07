8a6fde9c341db06fa70d0f474a119f9d213ae422
Sean Clark <sean@square-bracket.com>
2015-12-07 11:59:07 -0500

    comment back in the AssetFileDescriptor class

--- src/android/NativeAudio.java
+++ src/android/NativeAudio.java
@@ -16,7 +16,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.content.Context;
-// import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.util.Log;
